3beb3fc79f33fdb1c549fe5eb633f50534b53cec
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-01-04 21:41:28 +0300

    Сделал кнопочки

--- modules/widget.py
+++ modules/widget.py
@@ -6,7 +6,11 @@ def start():
     # Прием сообщения с сайта
     @eel.expose
     def call_in_js(msg):
-        print(msg)
+        call_in_python(msg)
+
+    # Отправка сообщения на сайт
+    def call_in_python(msg):
+        eel.call_in_python(msg)
 
     eel.start("index.html", mode='chrome', size=(640, 400))
     #edge, opera, electron, custom, none, false
