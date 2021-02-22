a8211a853a615cf57239ee3158fdaa2e5cd54042
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-02-22 21:57:20 +0300

    конечная версия

--- modules/widget.py
+++ modules/widget.py
@@ -1,5 +1,6 @@
 import eel
 import os
+import webbrowser
 
 def start():
     eel.init("web")
@@ -24,6 +25,15 @@ def start():
         elif msg_in == 'osu':
             msg_out = 'Запуск osu'
             os.system('D:\GAME\Osu\osu!.exe')
+        elif msg_in == 'youtube':
+            msg_out = 'Запуск ютуба'
+            webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO')
+        elif msg_in == 'discord':
+            msg_out = 'Запуск дискорда'
+            os.system('C:/Users/Администратор/AppData/Local/Discord/Update.exe --processStart Discord.exe')
+        elif msg_in == 'weigartemp':
+            msg_out = 'Очистка темпа'
+            os.system('C:/Users/Администратор/Desktop/weigartemp.bat')
         else:
             msg_out = msg_in[::-1]
         call_in_python(msg_out)
